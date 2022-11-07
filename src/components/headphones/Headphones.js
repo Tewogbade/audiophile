26f0e10e579bd096a457dc6f6d9c7e71200d8643
Tewogbade <tewogbade3008@gmail.com>
2022-11-07 15:42:04 +0100

    headphones and speakers update

--- src/components/headphones/Headphones.js
+++ src/components/headphones/Headphones.js
@@ -1,7 +1,126 @@
 import React from "react";
+import "../headphones/Headphones.css";
+import headphonesMarkIIm from "../../images/headphones/product-xx99-mark-two-headphones/mobile/image-category-page-preview.jpg";
+import headphonesMarkIIt from "../../images/headphones/product-xx99-mark-two-headphones/tablet/image-category-page-preview.jpg";
+import headphonesMarkIId from "../../images/headphones/product-xx99-mark-two-headphones/desktop/image-category-page-preview.jpg";
+import headphonesMarkIm from "../../images/headphones/product-xx99-mark-one-headphones/mobile/image-category-page-preview.jpg";
+import headphonesMarkIt from "../../images/headphones/product-xx99-mark-one-headphones/tablet/image-category-page-preview.jpg";
+import headphonesMarkId from "../../images/headphones/product-xx99-mark-one-headphones/desktop/image-category-page-preview.jpg";
+import headphonesxx59m from "../../images/headphones/product-xx59-headphones/mobile/image-category-page-preview.jpg";
+import headphonesxx59t from "../../images/headphones/product-xx59-headphones/tablet/image-category-page-preview.jpg";
+import headphonesxx59d from "../../images/headphones/product-xx59-headphones/desktop/image-category-page-preview.jpg";
+import section2headphone from "../../images/home/shared/desktop/image-category-thumbnail-headphones.png";
+import section2speaker from "../../images/home/shared/desktop/image-category-thumbnail-speakers.png";
+import section2earphone from "../../images/home/shared/desktop/image-category-thumbnail-earphones.png";
+import arrowright from "../../images/home/shared/desktop/icon-arrow-right.svg";
 
 const Headphones = () => {
-  return <div>Headphones</div>;
+  return (
+    <>
+      <div className="header">
+        <h4>Headphones</h4>
+      </div>
+      <div className="headphones">
+        <div className="markII">
+          <div className="headphonesimages">
+            <img className="mobile" src={headphonesMarkIIm} alt="" />
+            <img className="tablet" src={headphonesMarkIIt} alt="" />
+            <img className="desktop" src={headphonesMarkIId} alt="" />
+          </div>
+          <div className="headphonestext">
+            <h6>New product</h6>
+            <h2>
+              XX99 Mark II <br /> Headphones
+            </h2>
+            <p>
+              The new XX99 Mark II headphones is the pinnacle of pristine audio.
+              It redefines your premium headphone experience by reproducing the
+              balanced depth and precision of studio-quality sound.
+            </p>
+            <a href="#" className="btn">
+              See product
+            </a>
+          </div>
+        </div>
+
+        <div className="markI">
+          <div className="headphonesimages">
+            <img className="mobile" src={headphonesMarkIm} alt="" />
+            <img className="tablet" src={headphonesMarkIt} alt="" />
+            <img className="desktop" src={headphonesMarkId} alt="" />
+          </div>
+          <div className="headphonestext">
+            <h6>New product</h6>
+            <h2>
+              XX99 Mark I <br /> Headphones
+            </h2>
+            <p>
+              As the gold standard for headphones, the classic XX99 Mark I
+              offers detailed and accurate audio reproduction for audiophiles,
+              mixing engineers, and music aficionados alike in studios and on
+              the go.
+            </p>
+            <a href="#" className="btn">
+              See product
+            </a>
+          </div>
+        </div>
+
+        <div className="xx59">
+          <div className="headphonesimages">
+            <img className="mobile" src={headphonesxx59m} alt="" />
+            <img className="tablet" src={headphonesxx59t} alt="" />
+            <img className="desktop" src={headphonesxx59d} alt="" />
+          </div>
+          <div className="headphonestext">
+            <h6>New product</h6>
+            <h2>
+              XX99 Mark I <br /> Headphones
+            </h2>
+            <p>
+              As the gold standard for headphones, the classic XX99 Mark I
+              offers detailed and accurate audio reproduction for audiophiles,
+              mixing engineers, and music aficionados alike in studios and on
+              the go.
+            </p>
+            <a href="#" className="btn">
+              See product
+            </a>
+          </div>
+        </div>
+
+        <div className="lastsec">
+          <div className="pair">
+            <img className="ximages" src={section2headphone} alt="" />
+            <div className="greybox">
+              <h6>Headphones</h6>
+              <a href="#">
+                Shop <img src={arrowright} alt="" />
+              </a>
+            </div>
+          </div>
+          <div className="pair">
+            <img className="ximages" src={section2speaker} alt="" />
+            <div className="greybox">
+              <h6>Speakers</h6>
+              <a href="#">
+                Shop <img src={arrowright} alt="" />
+              </a>
+            </div>
+          </div>
+          <div className="pair">
+            <img className="ximages" src={section2earphone} alt="" />
+            <div className="greybox">
+              <h6>Earphones</h6>
+              <a href="#">
+                Shop <img src={arrowright} alt="" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Headphones;
--- src/components/speakers/Speakers.js
+++ src/components/speakers/Speakers.js
@@ -1,7 +1,103 @@
 import React from "react";
+import "../speakers/Speakers.css";
+import speakersZx9m from "../../images/speakers/product-zx9-speaker/mobile/image-category-page-preview.jpg";
+import speakersZx9t from "../../images/speakers/product-zx9-speaker/tablet/image-category-page-preview.jpg";
+import speakersZx9d from "../../images/speakers/product-zx9-speaker/desktop/image-category-page-preview.jpg";
+
+import speakersZx7m from "../../images/speakers/product-zx7-speaker/mobile/image-category-page-preview.jpg";
+import speakersZx7t from "../../images/speakers/product-zx7-speaker/tablet/image-category-page-preview.jpg";
+import speakersZx7d from "../../images/speakers/product-zx7-speaker/desktop/image-category-page-preview.jpg";
+
+import section2headphone from "../../images/home/shared/desktop/image-category-thumbnail-speakers.png";
+import section2speaker from "../../images/home/shared/desktop/image-category-thumbnail-speakers.png";
+import section2earphone from "../../images/home/shared/desktop/image-category-thumbnail-earphones.png";
+import arrowright from "../../images/home/shared/desktop/icon-arrow-right.svg";
 
 const Speakers = () => {
-  return <div>Speakers</div>;
+  return (
+    <div>
+      <div className="header">
+        <h4>speakers</h4>
+      </div>
+      <div className="speakers">
+        <div className="Zx9">
+          <div className="speakersimages">
+            <img className="mobile" src={speakersZx9m} alt="" />
+            <img className="tablet" src={speakersZx9t} alt="" />
+            <img className="desktop" src={speakersZx9d} alt="" />
+          </div>
+          <div className="speakerstext">
+            <h6>New product</h6>
+            <h2>
+              ZX9 <br /> speaker
+            </h2>
+            <p>
+              Upgrade your sound system with the all new ZX9 active speaker.
+              It’s a bookshelf speaker system that offers truly wireless
+              connectivity -- creating new possibilities for more pleasing and
+              practical audio setups.
+            </p>
+            <a href="#" className="btn">
+              See product
+            </a>
+          </div>
+        </div>
+
+        <div className="Zx7">
+          <div className="speakersimages">
+            <img className="mobile" src={speakersZx7m} alt="" />
+            <img className="tablet" src={speakersZx7t} alt="" />
+            <img className="desktop" src={speakersZx7d} alt="" />
+          </div>
+          <div className="speakerstext">
+            <h6>New product</h6>
+            <h2>
+              ZX7 <br /> speaker
+            </h2>
+            <p>
+              Stream high quality sound wirelessly with minimal loss. The ZX7
+              bookshelf speaker uses high-end audiophile components that
+              represents the top of the line powered speakers for home or studio
+              use.
+            </p>
+            <a href="#" className="btn">
+              See product
+            </a>
+          </div>
+        </div>
+
+        <div className="lastsec">
+          <div className="pair">
+            <img className="ximages" src={section2headphone} alt="" />
+            <div className="greybox">
+              <h6>speakers</h6>
+              <a href="#">
+                Shop <img src={arrowright} alt="" />
+              </a>
+            </div>
+          </div>
+          <div className="pair">
+            <img className="ximages" src={section2speaker} alt="" />
+            <div className="greybox">
+              <h6>Speakers</h6>
+              <a href="#">
+                Shop <img src={arrowright} alt="" />
+              </a>
+            </div>
+          </div>
+          <div className="pair">
+            <img className="ximages" src={section2earphone} alt="" />
+            <div className="greybox">
+              <h6>Earphones</h6>
+              <a href="#">
+                Shop <img src={arrowright} alt="" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Speakers;
